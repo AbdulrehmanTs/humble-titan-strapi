344632d02a41a203cce231c25436a1eb6e934276
AbdulrehmanTs <rohi644@gmail.com>
2022-06-24 20:11:39 +0500

    population

--- src/api/about/controllers/about.js
+++ src/api/about/controllers/about.js
@@ -6,4 +6,43 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::about.about');
+module.exports = createCoreController('api::about.about', ({ strapi }) => ({
+    async find(ctx) {
+        const { query } = ctx;
+
+        const entity = await strapi.entityService.findMany('api::about.about', {
+            ...query,
+            populate: {
+                hero: {
+                    populate: '*'
+                },
+                section_with_image: {
+                    populate: '*'
+                },
+                trustedByCompanies: {
+                    populate: '*'
+                },
+                ourValue_section: {
+                    populate: '*'
+                },
+                meetTheTeam_section: {
+                    populate: '*'
+                },
+                section_with_image_left: {
+                    populate: '*'
+                },
+                ourOffices: {
+                    populate: '*'
+                },
+                ht_digital_services: {
+                    populate: '*'
+                },
+            },
+        });
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+        return this.transformResponse(sanitizedEntity);
+
+    }
+})
+);
